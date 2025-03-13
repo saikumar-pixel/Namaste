8863ad246a97a6cf81bd1c109aeb2822eda1cd17
saikumar-pixel <saikumarmogilla@gmail.com>
2025-03-13 23:43:16 +0530

    commited succesfully

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "Hello world I am"; // her we get this, because file treats it as script rathe rthan module, module has own scope where script has global scope.
+console.log(message);
+var name = 'Hey this is mogilla';
+console.log(name);
---
+++ main.ts
@@ -0,0 +1,7 @@
+export {}
+
+let message="Hello world I am";// her we get this, because file treats it as script rathe rthan module, module has own scope where script has global scope.
+console.log(message);
+let name='Hey this is mogilla';
+
+console.log(name);
